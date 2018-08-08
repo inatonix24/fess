948c05828b6f9af213233eac6a1ea0662788edad
Hiroyuki Inada <inada_hiroyuki@jp.fujitsu.com>
2018-08-08 12:02:25 +0900

    debug

--- check_http_response.sh
+++ check_http_response.sh
@@ -4,7 +4,7 @@ check=true
 while [ $check ]
 do
   echo check response
-  check=`curl http://localhost | grep "Connection refused"''
+  check=`curl http://localhost | grep "Connection refused"`
   echo $check
   # curl -LI http://localhost -o /dev/null -w '%{http_code}\n' -s
   # code=`curl -LI http://localhost -o /dev/null -w '%{http_code}\n' -s`
