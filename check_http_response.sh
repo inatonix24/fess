5fa285c8e1cc0bb12db0879005f9f72adb385953
Hiroyuki Inada <inada_hiroyuki@jp.fujitsu.com>
2018-08-08 11:48:41 +0900

    debug

--- check_http_response.sh
+++ check_http_response.sh
@@ -4,6 +4,7 @@ code=400
 while [ "$code" -ge "400" ]
 do
   echo check response
+  curl http://localhost
   code=`curl -LI http://localhost -o /dev/null -w '%{http_code}\n' -s`
   echo response code = $code
   sleep 0.5
